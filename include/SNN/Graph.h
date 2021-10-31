ab41b0ca3e7ff678d55cbc084ec80847d72fb607
seandburke99 <seandburke99@outlook.com>
2021-10-31 20:55:57 -0300

    Added more comments

--- include/SNN/Graph.h
+++ include/SNN/Graph.h
@@ -25,15 +25,47 @@ typedef struct Graph{
     uint16_t numLayers;     //Counter for the number of layers in a network
 } Graph;
 
+/*
+ * Forward function for a graph structure to feed data through
+ * model: pointer to graph structure to feed data through
+ * data: pointer to data to pass through graph structure
+ *
+ * returns: pointer to graph output
+ */
 int16_t* forward(const Graph* model, const int16_t* data);
 #endif
 
+/*
+ * Function that creates a new graph structure and initializes
+ * all graph data to 0 or NULL
+ *
+ * returns: pointer to new graph structure
+ */
 Graph* create_graph();
 
+/*
+ * Function to free all memory associated with a graph structure
+ * model: pointer to model that needs to be freed
+ *
+ * returns: n/a
+ */
 void destroy_graph(Graph* model);
 
-int8_t add_layer(Graph* model, Layer l);
+/*
+ * Function to add a layer to a graph structure
+ * model: pointer to graph structure to add the layer to
+ * l: layer structure to 
+ *
+ * returns: 0 if successful, otherwise if not
+ */
+int8_t add_layer(Graph* model, const Layer l);
 
+/*
+ * Function to print out full graph structure
+ * model: pointer to graph to print out
+ *
+ * returns: n/a
+ */
 void graph_summarize(const Graph* model);
 
 #endif
