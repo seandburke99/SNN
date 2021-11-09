e23c34f5dbdd8d524b67086d731e3ccacda01fe4
seandburke99 <seandburke99@outlook.com>
2021-11-09 12:17:08 -0600

    Fixed feed foward function

--- src/Layer.c
+++ src/Layer.c
@@ -9,15 +9,11 @@ SNNFTYPE* layer_forward(const Layer* l, const SNNFTYPE* data){
     for(int i=0;i<l->dims[0];i++) printf("%f ", data[i]);
     printf("\n");
     for(int i=0;i<l->dims[1];i++){
-        _data[i] = 0;
+        _data[i] = (double)l->bias[i];
         for(int j=0;j< l->dims[0];j++){
             index = (i*offset) + j;
-            _data[i] += data[i]*l->weights[index];
-            printf("%f ", _data[i]);
-            //printf("%0.6f ",l->weights[index]);
+            _data[i] += data[j]*l->weights[index];
         }
-        printf("\n");
-        //printf("l->bias[%d] = %0.4f \n", i, l->bias[i]);
     }
     l->act(_data, l->dims[1]);
     return _data;
